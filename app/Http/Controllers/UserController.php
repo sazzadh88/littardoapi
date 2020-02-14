861d9dc33ab8cfa9dd3143d4eee8b570c1f264c8
sazzadh88 <sazzadh88@gmail.com>
2020-02-14 12:58:31 +0530

    a

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\BusinessSetting;
+use App\Customer;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        return $request->all();
+        $user = User::create([
+            'name' => $request->name,
+            'email' =>$request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $customer = new Customer();
+        $customer->user_id = $user->id;
+        $customer->save();
+
+        if(BusinessSetting::where('type', 'email_verification')->first()->value != 1){
+            $user->email_verified_at = date('Y-m-d H:m:s');
+            $user->save();
+           return response()->json(['code' => 200, 'message' => 'Registration successful']);
+        }
+        else {
+            return response()->json(['code' => 200, 'message' => 'Registration successful. Please verify your email.']);
+        }
     }
 }
