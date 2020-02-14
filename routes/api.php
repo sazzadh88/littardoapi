6f39302cd8718fa551b4511fd0c27896f536bb51
sazzadh88 <sazzadh88@gmail.com>
2020-02-14 15:03:39 +0530

    a

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('banners','AppController@banners');
+Route::get('banners','AppController@banners');
 Route::group(['prefix' => 'user'], function () {
 
     Route::post('register','UserController@register');
